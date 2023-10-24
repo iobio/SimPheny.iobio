f3262c3ff9cf6fd222576b808eaed1a2d2423f23
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-10-24 17:00:54 -0600

    Create left bar and action
    Created the left bar and the collapse functionality. Attempted to use a drawer but this limited my ability to more freely add items into it.

---
+++ src/components/LeftBar.vue
@@ -0,0 +1,59 @@
+<script>
+    export default {
+        name: 'LeftBar',
+        props: {
+            showLeftBar: {
+                type: Boolean,
+                default: true
+            }
+            
+        },
+        data: function() {
+            return {
+            
+            }
+        },
+        methods: {
+
+        },
+    }
+</script>
+
+<template>
+    <div id="left-bar-container" :class="{ expanded: showLeftBar, collapsed: !showLeftBar}">
+        <v-btn icon @click="showLeftBar = !showLeftBar" class="btn umbrella">
+            <v-tooltip offset="3" location="bottom" activator="parent">
+                <span v-if="!showLeftBar">Open patient details</span>
+                <span v-if="showLeftBar">Close patient details</span>
+            </v-tooltip>
+            <v-icon v-if="showLeftBar">mdi-umbrella-outline</v-icon>
+            <v-icon v-if="!showLeftBar">mdi-umbrella-closed-outline</v-icon>
+        </v-btn>
+    </div>
+</template>
+
+<style>
+    #left-bar-container {
+        position: relative;
+        height: 100%;
+        transition: all .5s ease-in-out;
+    }
+
+    #left-bar-container.collapsed {
+        width: 60px;
+        border-right: 2px solid transparent;
+    }
+    #left-bar-container.expanded {
+        width: 350px;
+        border-right: 2px solid #21351f;
+        box-shadow: 5px 0px 5px -2px rgba(0,0,0,0.2);
+    }
+    .btn.umbrella {
+        z-index: 9990;
+        margin-top: 52px;
+        margin-right: 5px;
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,45 +1,34 @@
 <script>
+    import LeftBar from './LeftBar.vue'
+
     export default {
         name: 'NavBar',
+        components: {
+            LeftBar,
+        },
         props: {
             
         },
         data: function() {
             return {
-                hide: false,
-                collapseTooltipOpen: false,
+
             }
         },
         methods: {
-            toggleCollapseToolbar: function() {
-                this.hide = !this.hide;
-                this.collapseTooltipOpen = false;
-            },
+
         },
     }
 </script>
 
 <template>
     <div id="nav-bar">
-        <v-toolbar density="compact" color="#21351f" :collapse="hide">
-            <v-btn icon @click="">
-                <v-tooltip offset="3" location="bottom" activator="parent">
-                    <span>Show patient details</span>
-                </v-tooltip>
-                <v-icon>mdi-menu</v-icon>
-            </v-btn>
-
+        <v-app-bar density="compact" color="#21351f" >
             <v-toolbar-title>Pheno-Matcher.iobio</v-toolbar-title>
-
-            <v-btn icon @click="toggleCollapseToolbar">
-                <v-tooltip offset="3" v-model="collapseTooltipOpen" location="bottom" activator="parent">
-                    <span>Collapse</span>
-                </v-tooltip>
-                <v-icon v-if="hide == false">mdi-menu-left-outline</v-icon>
-                <v-icon v-if="hide == true">mdi-menu-right-outline</v-icon>
-            </v-btn>
-        </v-toolbar>
+        </v-app-bar>
     </div>
+
+    <LeftBar></LeftBar>
+
 </template>
 
 <style>
