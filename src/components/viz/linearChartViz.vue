83b95e08e8071162b846678d3ac6dbd42dd1e1ec
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-10-30 16:57:39 -0600

    Make changes for resizing
    Made changes for resize observer to fix issue that happened when the chart did not have data.

--- src/components/viz/linearChartViz.vue
+++ src/components/viz/linearChartViz.vue
@@ -1,6 +1,10 @@
 <template>
     <div id="linear-chart-container">
-        <div ref="lin-chart-container" id="lin-chart-viz" @click="selectMatch"></div>
+        <div ref="lin-chart-container" id="lin-chart-viz" @click="selectMatch" v-if="targetPatient"></div>
+        <div v-else id="lin-chart-alt-text">
+            <p>No target patient defined.</p>
+            <p>Input target patient to view matches.</p>
+        </div>
         <div id="lin-chart-tip"></div>
     </div>
 
@@ -21,26 +25,41 @@
         data: function() {
             return {
                 linearChart: null,
+                resizeObserver: null,
             }
         },
         mounted() {
-            var linChartContainer = this.$refs['lin-chart-container'];
+            if (this.targetPatient) {
+                var linChartContainer = this.$refs['lin-chart-container'];
 
-            //add a listener for when the window is resized or the container is resized
-            const resizeObserver = new ResizeObserver(() => {
+                //add a listener for when the window is resized or the container is resized
+                this.resizeObserver = new ResizeObserver(() => {
+                    this.drawChart();
+                });
+
+                this.resizeObserver.observe(linChartContainer);
+
                 this.drawChart();
-            });
-
-            resizeObserver.observe(linChartContainer);
-
-            this.drawChart();
+            }
         },
         updated() {
-            this.drawChart();
+            if (this.targetPatient) {
+                if (this.resizeObserver == null) {
+                    var linChartContainer = this.$refs['lin-chart-container'];
+                    //add a listener for when the window is resized or the container is resized
+                    this.resizeObserver = new ResizeObserver(() => {
+                        this.drawChart();
+                    });
+                    this.resizeObserver.observe(linChartContainer);
+                }
+                this.drawChart();
+            }
         },
         beforeDestroy() {
-            //remove the resize observer
-            resizeObserver.unobserve(linChartContainer);
+            //remove the resize observer if it exists
+            if (this.resizeObserver != null) {
+                this.resizeObserver.unobserve(linChartContainer);
+            }
         },
         methods: {
             drawChart() {
@@ -73,39 +92,45 @@
 
 </script>
 
-<style>
-    #linear-chart-container {
-        height: 90%;
-        max-height: 500px;
-        width: 80%;
-        background-color: rgb(255, 255, 255);
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
+<style lang="sass">
+    #linear-chart-container 
+        height: 90%
+        max-height: 500px
+        width: 80%
+        background-color: rgb(255, 255, 255)
+        display: flex
+        flex-direction: column
+        justify-content: center
 
-        border-radius: 5px;
-    }
-    #lin-chart-viz {
-        height: 100%;
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
-    #lin-chart-tip {
-        position: absolute;
-        visibility: hidden;
-        background-color: rgba(0, 0, 0, 0.8);
-        color: white;
-        padding: 5px;
-        border-radius: 5px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: start;
-        height: fit-content;
-        width: fit-content;
+        border-radius: 5px
+        #lin-chart-viz 
+            height: 100%
+            width: 100%
+            display: flex
+            flex-direction: column
+            justify-content: center
+        #lin-chart-alt-text 
+            height: 100%
+            width: 100%
+            display: flex
+            flex-direction: column
+            justify-content: center
+            p
+                text-align: center
+                font-size: large
+        #lin-chart-tip 
+            position: absolute
+            visibility: hidden
+            background-color: rgba(0, 0, 0, 0.8)
+            color: white
+            padding: 5px
+            border-radius: 5px
+            display: flex
+            flex-direction: column
+            justify-content: center
+            align-items: start
+            height: fit-content
+            width: fit-content
 
-        font-size: small;
-    }
+            font-size: small 
 </style>
