e32d7c9c6db1f51cbdc2f8e77e7fce1b777affd8
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-11-28 13:22:29 -0700

    Refactor popout component
    Refactored the key popout component of the chart.

---
+++ src/components/ChartKeyPopout.vue
@@ -0,0 +1,67 @@
+<template>
+    <p id="chart-key-popout" :class="{show: showChartKey}">
+    <span>
+        <svg height="8" width="8">
+            <rect x="1" y="1" width="6" height="6" style="fill:rgb(218, 180, 237); stroke: rgb(116, 4, 172);"></rect>
+        </svg>
+        = Diagnosed
+    </span>
+    <span>
+        <svg height="8" width="8">
+            <rect x="1" y="1" width="6" height="6" style="fill:rgb(9, 9, 9); stroke: rgb(14, 14, 14);"></rect>
+        </svg>
+        = Clinical Dx Only
+    </span>
+    <span>
+        <svg height="8" width="8">
+            <circle cx="4" cy="4" r="3" style="fill:rgb(189, 74, 247); stroke: rgb(116, 4, 172);"></circle>
+        </svg>
+        = Undiagnosed
+    </span>
+    <span>
+        <svg height="10" width="10">
+            <circle cx="5" cy="5" r="4" style="fill:rgb(201, 153, 225); stroke: rgb(116, 4, 172);"></circle>
+        </svg> /
+        <svg height="10" width="10">
+            <rect x="1" y="1" width="8" height="8" style="fill:rgb(201, 153, 225); stroke: rgb(116, 4, 172);"></rect>
+        </svg>
+        = Genes in Common
+    </span>
+</p>
+</template>
+
+<script>
+export default {
+    name: 'ChartKeyPopout',
+    props: {
+        showChartKey: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+<style lang="sass">
+    #chart-key-popout
+        display: flex
+        flex-direction: column
+        justify-content: center
+        align-items: start
+        font-size: 9pt
+        border-radius: 5px
+        width: 0px
+        height: 0px
+        padding: 0px
+        overflow: hidden
+        position: absolute
+        top: 18px
+        right: 18px
+        transition: all .45s ease-in-out
+    #chart-key-popout.show
+        height: 100px
+        width: 160px
+        padding: 5px
+        border: 1px solid #E9EDEA
+        color: #21351F
+</style>
--- src/components/viz/ChartViz.vue
+++ src/components/viz/ChartViz.vue
@@ -2,35 +2,7 @@
     <div id="linear-chart-container">
         <div id="chart-key-container">
             <p id="chart-key-hoverable" @mouseenter="showChartKey = true" @mouseleave="showChartKey = false"><v-icon>mdi-key-variant</v-icon></p>
-            <p id="chart-key-popout" :class="{show: showChartKey}">
-                <span>
-                    <svg height="8" width="8">
-                        <rect x="1" y="1" width="6" height="6" style="fill:rgb(218, 180, 237); stroke: rgb(116, 4, 172);"></rect>
-                    </svg>
-                    = Diagnosed
-                </span>
-                <span>
-                    <svg height="8" width="8">
-                        <rect x="1" y="1" width="6" height="6" style="fill:rgb(9, 9, 9); stroke: rgb(14, 14, 14);"></rect>
-                    </svg>
-                    = Clinical Dx Only
-                </span>
-                <span>
-                    <svg height="8" width="8">
-                        <circle cx="4" cy="4" r="3" style="fill:rgb(189, 74, 247); stroke: rgb(116, 4, 172);"></circle>
-                    </svg>
-                    = Undiagnosed
-                </span>
-                <span>
-                    <svg height="10" width="10">
-                        <circle cx="5" cy="5" r="4" style="fill:rgb(201, 153, 225); stroke: rgb(116, 4, 172);"></circle>
-                    </svg> /
-                    <svg height="10" width="10">
-                        <rect x="1" y="1" width="8" height="8" style="fill:rgb(201, 153, 225); stroke: rgb(116, 4, 172);"></rect>
-                    </svg>
-                    = Genes in Common
-                </span>
-            </p>
+            <ChartKeyPopout :showChartKey="showChartKey"></ChartKeyPopout>
         </div>
         <div ref="lin-chart-container" id="lin-chart-viz" v-if="targetPatient"></div>
         <div v-else id="lin-chart-alt-text">
@@ -124,11 +96,15 @@
 
 <script>
     import CircularChart from '../../d3/CircularChart.d3';
+    import ChartKeyPopout from '../ChartKeyPopout.vue';
     import * as d3 from 'd3';
 
     export default {
     emits: ['selectMatch'],
     name: 'ChartViz',
+    components: {
+        ChartKeyPopout
+    },
     props: {
         targetPatient: Object,
         patientMap: Object,
@@ -540,27 +516,6 @@
             border: 1px solid #21351F
         .v-icon
             font-size: 14pt
-    #chart-key-popout
-        display: flex
-        flex-direction: column
-        justify-content: center
-        align-items: start
-        font-size: 9pt
-        border-radius: 5px
-        width: 0px
-        height: 0px
-        padding: 0px
-        overflow: hidden
-        position: absolute
-        top: 18px
-        right: 18px
-        transition: all .45s ease-in-out
-    #chart-key-popout.show
-        height: 100px
-        width: 160px
-        padding: 5px
-        border: 1px solid #E9EDEA
-        color: #21351F
     #chart-options-btn
         background-color: #21351f
         position: absolute
