1a4102d1ee1c656ea5705c1e99b3b1bf4baa0bce
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-11-30 16:42:00 -0700

    Start refactoring
    Started refactoring the html

--- src/components/MatchesPane.vue
+++ src/components/MatchesPane.vue
@@ -16,19 +16,19 @@
                 <h1 v-if="selectedMatches && selectedMatches.length > 1" class="section-head">Selected Matches ({{ selectedMatches.length }})</h1>
                 <h1 v-if="!selectedMatches" class="section-head">Selected Match</h1>
                 <h4 v-if="selectedMatches && selectedMatches.length == 0" id="no-match-alt-text">No matches selected. Select matches from the chart to display details.</h4>
-                <div v-if="selectedMatches" class="column-container">
-                    <div v-if="selectedMatches && selectedMatches.length == 1" class="column">
-                        <h4>Summary</h4>
-                        <div id="summary-container">
-                            <p><b>ID:</b> {{ selectedMatches[0].id }}</p>
-                            <p><b>Rank:</b> {{ selectedMatches[0].rank }}</p>
-                            <p><b>Score:</b> {{ Math.round(selectedMatches[0].similarityScore * 10000)/10000 }}</p>
-                            <p><b>Dx Status:</b> {{ selectedMatches[0].dx }}</p>
-                            <p><b>Clinical Dx:</b> {{ selectedMatches[0].clinicalDiagnosis }}</p>
+
+                <div v-if="selectedMatches" class="column-container" :class="{ single: selectedMatches.length == 1 }">
+                    <div v-if="selectedMatches && selectedMatches.length == 1" class="column full-height">
+                        <div class="sub">
+                            <h4>Genes:</h4>
+                            <div>
+                                <p class="list-item inTarget" v-for="(value, key) in genesInCommon" :key="key">{{ value.gene.gene_symbol }}</p>
+                                <p class="list-item" v-for="(value, key) in genesNotInTarget" :key="key">{{ value.gene.gene_symbol }}</p>                              
+                            </div>
                         </div>
                     </div>
 
-                    <div v-if="selectedMatches && selectedMatches.length == 1" class="column">
+                    <div v-if="selectedMatches && selectedMatches.length == 1" class="column full-height">
                         <div class="sub">
                             <h4>Phenotypes</h4>
                             <div>
@@ -36,13 +36,16 @@
                                 <p class="list-item" v-for="(value, key) in phenNotInTarget" :key="key"> {{ value.phenotype.hpoId + " " + value.phenotype.term }}</p>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="sub">
-                            <h4>Genes:</h4>
-                            <div>
-                                <p class="list-item inTarget" v-for="(value, key) in genesInCommon" :key="key">{{ value.gene.gene_symbol }}</p>
-                                <p class="list-item" v-for="(value, key) in genesNotInTarget" :key="key">{{ value.gene.gene_symbol }}</p>                              
-                            </div>
+                    <div v-if="selectedMatches && selectedMatches.length == 1" class="column full-height">
+                        <h4>Summary</h4>
+                        <div id="summary-container">
+                            <p><b>ID:</b> {{ selectedMatches[0].id }}</p>
+                            <p><b>Rank:</b> {{ selectedMatches[0].rank }}</p>
+                            <p><b>Score:</b> {{ Math.round(selectedMatches[0].similarityScore * 10000)/10000 }}</p>
+                            <p><b>Dx Status:</b> {{ selectedMatches[0].dx }}</p>
+                            <p><b>Clinical Dx:</b> {{ selectedMatches[0].clinicalDiagnosis }}</p>
                         </div>
                     </div>
 
@@ -370,6 +373,10 @@
         max-width: 26%;
         min-width: 20%;
     }
+    .lower.matches .column-container.single .column:first-of-type {
+        max-width: 26%;
+        min-width: 20%;
+    }
     .lower.matches .column-container .column h4 {
         margin-bottom: 5px;
         margin-top: 10px;
@@ -470,6 +477,15 @@
     }
     .lower.matches .column-container .column.full-height:first-of-type .sub {
         height: 47%;
+    }
+    .lower.matches .column-container.single .column.full-height {
+        align-items: start;
+        justify-content: start;
+    }
+    .lower.matches .column-container.single .column.full-height:last-of-type {
+        width: 25%;
+        min-width: 20%;
+        max-width: 30%;
     }
     .lower.matches .column-container .column li {
         list-style-type: none;
