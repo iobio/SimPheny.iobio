bade87dcde9497acf4d2fa509f2d18e4a9bfd9b1
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-12-06 13:33:38 -0700

    Test with static_ic

--- phenoMatchBackend/Custom_Scoring.py
+++ phenoMatchBackend/Custom_Scoring.py
@@ -13,6 +13,9 @@ class CustomJaccardIC(SimilarityBase):
 
         if term1 == term2:
             return 1.0
+        
+        term1_ic = term1.information_content[kind]
+        term2_ic = term2.information_content[kind]
 
         common = sum([ x.information_content[kind] for x in term1.common_ancestors(term2) ])
         union = sum([ x.information_content[kind] for x in (term1.all_parents | term2.all_parents) ])
@@ -20,11 +23,11 @@ class CustomJaccardIC(SimilarityBase):
         if term1 in term2.all_parents and term2 in term1.all_parents:
             union_add = 0
         elif term1 in term2.all_parents:
-            union_add = term2.information_content[kind]
+            union_add = term2_ic
         elif term2 in term1.all_parents:
-            union_add = term1.information_content[kind]
+            union_add = term1_ic
         else:
-            union_add = (term1.information_content[kind] + term2.information_content[kind])
+            union_add = (term1_ic + term2_ic)
 
         union += union_add
         try:
