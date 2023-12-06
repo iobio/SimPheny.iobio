5a3f6bd53e0680ee4460d7834261d6e84b0f191b
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-12-06 13:50:30 -0700

    no list on union

--- phenoMatchBackend/Custom_Scoring.py
+++ phenoMatchBackend/Custom_Scoring.py
@@ -16,7 +16,7 @@ class CustomJaccardIC(SimilarityBase):
 
         common = sum(x.information_content[kind] for x in (term1.all_parents & term2.all_parents))
         # common = sum([ x.information_content[kind] for x in term1.common_ancestors(term2) ])
-        union = sum([ x.information_content[kind] for x in (term1.all_parents | term2.all_parents) ])
+        union = sum(x.information_content[kind] for x in (term1.all_parents | term2.all_parents))
 
 
         if term1 in term2.all_parents and term2 in term1.all_parents:
