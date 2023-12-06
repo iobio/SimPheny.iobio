c351420ec87b46af3451474fcb753b5d7a93f432
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-12-06 15:40:05 -0700

    Update Custom_Scoring.py

--- phenoMatchBackend/Custom_Scoring.py
+++ phenoMatchBackend/Custom_Scoring.py
@@ -17,15 +17,13 @@ class CustomJaccardIC(SimilarityBase):
         intersection_sum = sum(x.information_content[kind] for x in (term1.all_parents & term2.all_parents))
         union_sum = sum(x.information_content[kind] for x in (term1.all_parents | term2.all_parents))
 
-        # since being a parent is mutually exclusive only one of these will be true
+        # since being a parent is mutually exclusive only one of these will be true if either
         if term1 in term2.all_parents:
-            union_add = term2.information_content[kind]
+            union_sum += term2.information_content[kind]
         elif term2 in term1.all_parents:
-            union_add = term1.information_content[kind]
+            union_sum += term1.information_content[kind]
         else:
-            union_add = (term1.information_content[kind] + term2.information_content[kind])
-
-        union_sum += union_add
+            union_sum += (term1.information_content[kind] + term2.information_content[kind])
 
         try:
             return intersection_sum/union_sum
