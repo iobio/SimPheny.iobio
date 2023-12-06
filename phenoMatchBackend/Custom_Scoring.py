8630839f25ca2a8f120f4a0f452d9ebcdacda2fd
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-12-06 13:53:16 -0700

    Original

--- phenoMatchBackend/Custom_Scoring.py
+++ phenoMatchBackend/Custom_Scoring.py
@@ -15,7 +15,7 @@ class CustomJaccardIC(SimilarityBase):
             return 1.0
 
         common = sum([x.information_content[kind] for x in (term1.all_parents & term2.all_parents)])
-        union = sum(x.information_content[kind] for x in (term1.all_parents | term2.all_parents))
+        union = sum([x.information_content[kind] for x in (term1.all_parents | term2.all_parents)])
 
 
         if term1 in term2.all_parents and term2 in term1.all_parents:
