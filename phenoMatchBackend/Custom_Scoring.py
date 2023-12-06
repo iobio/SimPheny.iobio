a379b1205afaa87077dc5682ba54d95c865dd218
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-12-06 14:04:23 -0700

    Change back to set logic

--- phenoMatchBackend/Custom_Scoring.py
+++ phenoMatchBackend/Custom_Scoring.py
@@ -15,7 +15,7 @@ class CustomJaccardIC(SimilarityBase):
             return 1.0
 
         # common = sum(x.information_content[kind] for x in (term1.all_parents & term2.all_parents))
-        common = sum(x.information_content[kind] for x in term1.common_ancestors(term2))
+        common = sum(x.information_content[kind] for x in (term1.all_parents & term2.all_parents))
         union = sum(x.information_content[kind] for x in (term1.all_parents | term2.all_parents))
 
         if term1 in term2.all_parents:
