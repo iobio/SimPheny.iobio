11221be2ce15d1e7cb84130e78334f9c5009a9d0
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-02-27 12:45:38 -0700

    Test env

--- src/data/fetchFromBackend.js
+++ src/data/fetchFromBackend.js
@@ -1,5 +1,6 @@
 // var baseURL = "http://localhost:8911/"
-var baseURL = import.meta.env.VITE_APP_BACKEND_URL;
+// var baseURL = import.meta.env.VITE_APP_BACKEND_URL;
+var baseURL = 'https://mosaic-staging.chpc.utah.edu/phenomatcher-backend/'
 
 //url for compare data
 var compareURL = baseURL + "compare/"
