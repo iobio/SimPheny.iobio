459b84a0263f4ad76a0adc74381dd600663e3512
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2025-09-04 11:50:54 -0600

    Refactor transformPatientMap and updatePatientMap formatting
    Improved code readability by reformatting transformPatientMap and updatePatientMap functions with consistent indentation and spacing. No functional changes were made.

--- src/data/getData.js
+++ src/data/getData.js
@@ -1,68 +1,74 @@
-import { type } from "jquery";
 import MatchPatient from "../models/MatchPatient";
 import TargetPatient from "../models/TargetPatient";
 import * as Be from "./fetchFromBackend.js";
 
-export async function transformPatientMap(targetPatientId, targetTerms, targetGenes, simScoresObj, phenotypesMap, whichPopulation="udn"){
-  let patientMapRes = await Be.getPatientMap(whichPopulation);
+export async function transformPatientMap(
+    targetPatientId,
+    targetTerms,
+    targetGenes,
+    simScoresObj,
+    phenotypesMap,
+    whichPopulation = "udn",
+) {
+    let patientMapRes = await Be.getPatientMap(whichPopulation);
 
-  let patientMap = {};
-  let patientObject;
-  let simObject;
-  let targetGenesList = targetGenes;
+    let patientMap = {};
+    let patientObject;
+    let simObject;
+    let targetGenesList = targetGenes;
 
-  if (typeof targetGenes[0] !== 'string') {
-    targetGenesList = targetGenes.map(gene => gene.gene_symbol);
-  }
-
-  //first just generate and add the target patient
-  if (targetPatientId !== 'custom' && patientMapRes.hasOwnProperty(targetPatientId)) {
-    patientObject = patientMapRes[targetPatientId];
-    simObject = simScoresObj[targetPatientId];
-  } else {
-    patientObject = {"Dx/Udx": "None-Target", "Genes": targetGenesList, "Clin diagnosis": "None-Target", "Terms": targetTerms};
-    simObject = {"score": 1, "rank": 1};
-  }
-
-  let targetPatient = new TargetPatient(targetPatientId, patientObject, simObject);
-
-  targetPatient.setUserInputGenesList(targetGenesList);
-  targetPatient.setUserInputHpoIdList(targetTerms);
-  targetPatient.genPhenotypeList(phenotypesMap);
-  await targetPatient.genGenesList();
-
-  patientMap[targetPatientId] = targetPatient;
-
-  for(let patientId in patientMapRes){
-    if (patientMapRes.hasOwnProperty(patientId)){
-      if (patientId != targetPatientId){
-        let patientObject = patientMapRes[patientId];
-        let simObject = simScoresObj[patientId];
-
-        let matchPatient = new MatchPatient(patientId, patientObject, simObject);
-        matchPatient.genPhenotypeList(phenotypesMap);
-        matchPatient.genPhenotypesInCommon(targetPatient.getPhenotypeList());
-        await matchPatient.genGenesList();
-        matchPatient.genGenesInCommon(targetPatient.getGenesList());
-
-        patientMap[patientId] = matchPatient;
-      } else {
-        //skip the target patient
-      }
+    if (typeof targetGenes[0] !== "string") {
+        targetGenesList = targetGenes.map((gene) => gene.gene_symbol);
     }
-  }
-  return {"patientMap": patientMap, "targetPatient": targetPatient};
+
+    //first just generate and add the target patient
+    if (targetPatientId !== "custom" && patientMapRes.hasOwnProperty(targetPatientId)) {
+        patientObject = patientMapRes[targetPatientId];
+        simObject = simScoresObj[targetPatientId];
+    } else {
+        patientObject = { "Dx/Udx": "None-Target", Genes: targetGenesList, "Clin diagnosis": "None-Target", Terms: targetTerms };
+        simObject = { score: 1, rank: 1 };
+    }
+
+    let targetPatient = new TargetPatient(targetPatientId, patientObject, simObject);
+
+    targetPatient.setUserInputGenesList(targetGenesList);
+    targetPatient.setUserInputHpoIdList(targetTerms);
+    targetPatient.genPhenotypeList(phenotypesMap);
+    await targetPatient.genGenesList();
+
+    patientMap[targetPatientId] = targetPatient;
+
+    for (let patientId in patientMapRes) {
+        if (patientMapRes.hasOwnProperty(patientId)) {
+            if (patientId != targetPatientId) {
+                let patientObject = patientMapRes[patientId];
+                let simObject = simScoresObj[patientId];
+
+                let matchPatient = new MatchPatient(patientId, patientObject, simObject);
+                matchPatient.genPhenotypeList(phenotypesMap);
+                matchPatient.genPhenotypesInCommon(targetPatient.getPhenotypeList());
+                await matchPatient.genGenesList();
+                matchPatient.genGenesInCommon(targetPatient.getGenesList());
+
+                patientMap[patientId] = matchPatient;
+            } else {
+                //skip the target patient
+            }
+        }
+    }
+    return { patientMap: patientMap, targetPatient: targetPatient };
 }
 
-export async function updatePatientMap(newSimMap, oldPatientMap, newTargetGeneList=null) {
-  let updatedPatientMap = oldPatientMap;
-  for (let patientId in oldPatientMap) {
-    if (newSimMap.hasOwnProperty(patientId)) {
-      updatedPatientMap[patientId].setSimilarityScore(newSimMap[patientId].score);
-      if (newTargetGeneList !== null) {
-        updatedPatientMap[patientId].genGenesInCommon(newTargetGeneList);
-      }
+export async function updatePatientMap(newSimMap, oldPatientMap, newTargetGeneList = null) {
+    let updatedPatientMap = oldPatientMap;
+    for (let patientId in oldPatientMap) {
+        if (newSimMap.hasOwnProperty(patientId)) {
+            updatedPatientMap[patientId].setSimilarityScore(newSimMap[patientId].score);
+            if (newTargetGeneList !== null) {
+                updatedPatientMap[patientId].genGenesInCommon(newTargetGeneList);
+            }
+        }
     }
-  }
-  return updatedPatientMap;
+    return updatedPatientMap;
 }
