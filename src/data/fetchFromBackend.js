3a16ea46fc86fea9df12f2dcdbf921f71636d1f0
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2025-09-16 11:57:20 -0600

    Update fetchFromBackend.js

--- src/data/fetchFromBackend.js
+++ src/data/fetchFromBackend.js
@@ -1,5 +1,5 @@
-var baseURL = "http://localhost:8911/"; //for local testing
-// var baseURL = import.meta.env.VITE_APP_BACKEND_URL;
+// var baseURL = "http://localhost:8911/"; //for local testing
+var baseURL = import.meta.env.VITE_APP_BACKEND_URL;
 
 //url for compare data
 var compareURL_udn = baseURL + "compare_udn/";
